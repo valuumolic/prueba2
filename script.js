d80d63b81cb5345ee1baba530dbb931ad3b1aa58
valuumolic <valentina.molina.ttc.071@unilibre.edu.co>
2024-11-20 15:54:47 -0500

    a

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navLinks = document.querySelector('.nav-links');
-
-    menuToggle.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-
-    document.getElementById('nav-toggle').addEventListener('click', function() {
-        document.querySelector('nav').classList.toggle('nav-open');
-    });
+document.getElementById('nav-toggle').addEventListener('click', function() {
+    document.querySelector('nav').classList.toggle('nav-open');
 });
