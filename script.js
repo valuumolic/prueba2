a08174054b41ebb4d0938616fcb938f9e4dd27d7
valuumolic <valentina.molina.ttc.071@unilibre.edu.co>
2024-11-20 15:50:05 -0500

    a

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const navLinks = document.querySelector('.nav-links');
 
     menuToggle.addEventListener('click', () => {
-        navLinks.classList.toggle('[_{{{CITATION{{{_1{](https://github.com/la9una/web/tree/ba1073ae044ebb7b538a3b13f0f9598f7c410bb6/docs%2Fbootstrap%2Falignci.md)
+        navLinks.classList.toggle('active');
+    });
+
+    document.getElementById('nav-toggle').addEventListener('click', function() {
+        document.querySelector('nav').classList.toggle('nav-open');
+    });
+});
